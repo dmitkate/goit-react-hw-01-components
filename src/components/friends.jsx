43e9b3c1cc3aae858a34c2511cefa4851015faf4
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-10-18 19:50:13 +0300

    в

--- src/components/friends.jsx
+++ src/components/friends.jsx
@@ -1,18 +1,22 @@
-
 import css from '../components/frnds.module.css';
 export const FriendList = ({ friends }) => {
     return ( 
 <section className={css.statistics}>
         <ul>
      {friends.map(i => (
-        <li className={css.item} key={i.id}>
-             <span className={i.isOnline ? [css.online] : [css.notonline]}>{i.isOnline}</span>
-             <img class="avatar" src={i.avatar} alt="User avatar" width="48" heigth="48"/>
-             <p className={css.name}>{i.name}</p>
-        </li>
+        <FriendListItem  key={i.id} friend={i} />
       ))}
   
    </ul>
 </section>
     )
 };
+const FriendListItem = ({ friend }) => {
+    return (
+        <li className={css.item} key={friend.id}>
+             <span className={friend.isOnline ? [css.online] : [css.notonline]}>{friend.isOnline}</span>
+             <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" heigth="48"/>
+             <p className={css.name}>{friend.name}</p>
+        </li>
+    )
+}
--- src/components/statistics.jsx
+++ src/components/statistics.jsx
@@ -1,8 +1,9 @@
 import css from '../components/stt.module.css';
-export const Statistics = ({ stats }) => {
+export const Statistics = ({title, stats }) => {
     return (
 <section className={css.statistics}>
-  <h2 className={css.title}>Upload stats</h2>
+            <h2 className={css.title}>{title}</h2>
+          
   <ul className={css.statlist}>
      {stats.map(i => (
          <li key={i.id}>
